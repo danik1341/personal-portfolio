b063359bd96bfe51a08ba526bd974fc0cefe2a02
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-04-20 18:26:11 +0300

    project schema made

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from 'sanity/desk';
+import project from "./sanity/schemas/project-schema";
 
 
 // Sanity sever configuration
@@ -9,7 +10,8 @@ const config = defineConfig({
     title: 'My Personal Website',
     apiVersion: '2023-20-04',
     basePath: '/admin',
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: { types: [project] }
 })
 
 export default config;
