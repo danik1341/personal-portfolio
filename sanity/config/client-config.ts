b71e1f62cfdf9c18cde8f60825471c9746fd458a
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-04-20 22:59:05 +0300

    restructured sanity hoooks

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+    projectId: 'hh6ywlyw',
+    dataset: 'production',
+    apiVersion: '2023-04-20',
+}
+
+export default config;
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,13 +1,9 @@
 import { Project } from '@/types/Project';
 import { createClient, groq } from 'next-sanity'
+import clientConfig from './config/client-config';
 
 export async function getProjects(): Promise<Project[]> {
-    const client = createClient({
-        projectId: 'hh6ywlyw',
-        dataset: 'production',
-        apiVersion: '2023-04-20',
-    });
-    return client.fetch(
+    return createClient(clientConfig).fetch(
         groq`*[_type == "project"]{
             _id,
             _createdAt,
@@ -19,3 +15,18 @@ export async function getProjects(): Promise<Project[]> {
           }`
     )
 }
+
+export async function getProject(slug: string): Promise<Project> {
+    return createClient(clientConfig).fetch(
+      groq`*[_type == "project" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        url,
+        content
+      }`,
+      { slug }
+    )
+  }
